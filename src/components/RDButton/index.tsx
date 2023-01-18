65f3080bc42fa83fcb61a35aebe2e546d0184c39
Ronny Wu <ronny.wu@welab.co.od>
2023-01-18 17:52:26 +0800

    feat: 完善文档

--- src/components/RDButton/index.tsx
+++ src/components/RDButton/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-export interface Props { 
+export interface Props {
   /**
    * @description 背景颜色
    * @default
